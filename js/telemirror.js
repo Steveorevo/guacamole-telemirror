dfee1032d832b2f63df26068eadde68723e43217
Stephen J Carnam <steveorevo@gmail.com>
2023-04-20 12:44:03 -0700

    wip

--- js/telemirror.js
+++ js/telemirror.js
@@ -1,4 +1,10 @@
-setInterval(function() {
-    console.log("Hello from telemirror");
-}, 5000);
-
+// Check for mirror paramter or title ending in _mirror
+(function($) {
+    $(function() {
+        if (window.document.title.endsWith('_mirror') || window.location.toString().indexOf('mirror=true') != -1) {
+            $('#content .viewport .display canvas').css('transform', 'scaleX(-1)');
+        }else{
+            $('#content .viewport .display canvas').css('transform', 'scaleX(1)');
+        }
+    });
+})(jQuery);
